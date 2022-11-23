4426cf89eba774241d04267e4e6e34bc189dd7c6
larysyx <lauragabriela.lemos3@gmail.com>
2022-11-23 13:48:24 -0300

    backend

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node build/server.js
+relase: node build/ace migration:run --force
--- config/database.ts
+++ config/database.ts
@@ -61,22 +61,25 @@
      |
      */
      pg: {
-       client: 'pg',
-       connection: {
-         host: Env.get('PG_HOST'),
-         port: Env.get('PG_PORT'),
-         user: Env.get('PG_USER'),
-         password: Env.get('PG_PASSWORD', ''),
-         database: Env.get('PG_DB_NAME'),
-       },
-       migrations: {
-         naturalSort: true,
-       },
-       healthCheck: false,
-       debug: false,
-     },
- 
-   }
- }
- 
- export default databaseConfig
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false
+        }
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+  }
+}
+
+export default databaseConfig
